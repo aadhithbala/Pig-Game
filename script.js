1b1fd0a9264abc8c6dd2c3bd3fe1d7a8b2942069
aadhithbala <aadhi66613@gmail.com>
2023-07-12 23:59:21 +0530

    Update Switch player fn

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 'use strict';
 
 //Capturing all the elements in a variable
-let score = 0;
+let currentScore = 0;
 let activePlayer = 0;
 
 const score0 = document.getElementById('score--0');
 const score1 = document.getElementById('score--1');
-const currentScore0 = document.getElementById('current--0');
-const currentScore1 = document.getElementById('current--1');
 const dice = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+const player0 = document.querySelector('.player--0');
+const player1 = document.querySelector('.player--1');
 
 const prepareGameStart = function () {
   score0.textContent = 0;
@@ -21,7 +21,14 @@ const prepareGameStart = function () {
 
 const generateRandomDice = () => Math.floor(Math.random() * 6 + 1);
 
-const switchPlayer = function () {};
+const switchPlayer = function () {
+  currentScore = 0;
+  document.getElementById(`current--${activePlayer}`).textContent =
+    currentScore;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0.classList.toggle('player--active');
+  player1.classList.toggle('player--active');
+};
 
 prepareGameStart(); //Invoking function to remove scores
 
@@ -32,9 +39,10 @@ btnRoll.addEventListener('click', function () {
   dice.src = `./images/dice-${diceNumber}.png`;
 
   if (diceNumber !== 1) {
-    score += diceNumber;
-    document.getElementById(`current--${activePlayer}`).textContent = score;
+    currentScore += diceNumber;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
-    activePlayer = activePlayer === 0 ? 1 : 0;
+    switchPlayer();
   }
 });
