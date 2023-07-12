cb462fe026c7841a050f1dfba8831aefb91b1d58
aadhithbala <aadhi66613@gmail.com>
2023-07-12 22:57:30 +0530

    Add a function to prepare game & Add hidden css class to hide

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ const score1 = document.getElementById('score--1');
 const currentScore0 = document.getElementById('current--0');
 const currentScore1 = document.getElementById('current--1');
 const dice = document.querySelector('.dice');
-const btnNew = document.querySelector('btn--new');
-const btnRoll = document.querySelector('btn--roll');
-const btnHold = document.querySelector('btn--hold');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+const prepareGameStart = function () {
+  score0.textContent = 0;
+  score1.textContent = 0;
+  dice.classList.add('hidden');
+};
