c25bafe71ab27820ee806678b937c8452415b111
aadhithbala <aadhi66613@gmail.com>
2023-07-12 22:35:15 +0530

    Capture all the elements in a variable

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+//Capturing all the elements in a variable
+
+const score0 = document.getElementById('score--0');
+const score1 = document.getElementById('score--1');
+const currentScore0 = document.getElementById('current--0');
+const currentScore1 = document.getElementById('current--1');
+const dice = document.querySelector('.dice');
+const btnNew = document.querySelector('btn--new');
+const btnRoll = document.querySelector('btn--roll');
+const btnHold = document.querySelector('btn--hold');
